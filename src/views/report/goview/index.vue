6dec8e8e2e5c513e8ce116c257af17229fe54004
tianyu <tianyu@192.168.0.101>
2024-09-21 20:35:27 +0800

    init project

---
+++ src/views/report/goview/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <ContentWrap>
+    <doc-alert title="大屏设计器" url="https://doc.iocoder.cn/report/screen/" />
+
+    <IFrame :src="src" />
+  </ContentWrap>
+</template>
+<script lang="ts" setup>
+defineOptions({ name: 'GoView' })
+
+const src = 'http://127.0.0.1:3000'
+</script>
